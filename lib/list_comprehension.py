4fd5fa133a2e14f5dbf5c21bf992d6ad6c50d6d4
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-05-20 16:14:10 +0300

    completed assignments

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    list_even = []
+    return [num for num in num_list if num % 2 ==0]
+    print (list_even)
 
 def make_exclamation(sentence_list):
-    pass
+   return [sentence + '!' for sentence in sentence_list]
+   
